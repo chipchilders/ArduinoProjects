c5a431c51819b924a076c5c4a41de03c717773c5
Chip Childers <chip.childers@gmail.com>
2014-01-04 11:58:48 -0500

    Updated servo example to include LED blink on servo move command

--- ServoPlay/src/sketch.ino
+++ ServoPlay/src/sketch.ino
@@ -11,21 +11,48 @@ int pos = 0;    // variable to store the servo position
 int sensorPin = 0;    // select the input pin for the potentiometer
 float sensorValue = 0;  // variable to store the value coming from the sensor
 float servoTarget = 0;
-float servoRawTarget = 0;
+int lastServoTarget = 0;
+
+// LED leads connected to PWM pins
+const int RED_LED_PIN = 9;
+const int GREEN_LED_PIN = 10;
+const int BLUE_LED_PIN = 11;
+const int VERYBLUE_LED_PIN = 2;
+
+// Used to store the current intensity level of the individual LEDs
+int redIntensity = 0;
+int greenIntensity = 0;
+int blueIntensity = 0;
+
+// Length of time we spend showing each color
+const int DISPLAY_TIME = 100; // In milliseconds
 
 void setup() 
 { 
   Serial.begin(9600);
   myservo.attach(5);  // attaches the servo on pin 9 to the servo object 
+  pinMode(VERYBLUE_LED_PIN,OUTPUT);
+}
+
+void blinkblue(int count){
+  for (int i=0; i<count; i++){
+    digitalWrite(VERYBLUE_LED_PIN, HIGH);
+    delay(DISPLAY_TIME);
+    digitalWrite(VERYBLUE_LED_PIN, LOW);
+    delay(DISPLAY_TIME);
+  }
 } 
- 
- 
-void loop() 
+
+void loop()
 {
-  sensorValue = analogRead(sensorPin)+1;
+  sensorValue = analogRead(sensorPin);
   servoTarget = (sensorValue/1024)*180;
   Serial.println(servoTarget);
   myservo.write(servoTarget);
+  if (lastServoTarget != int(servoTarget)){
+    blinkblue(1);
+  }
+  lastServoTarget = servoTarget;
   delay(15);
 
 
