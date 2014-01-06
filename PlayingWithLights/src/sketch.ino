5c127acc9077c41cb15932af8c2b2a491c912af7
Chip Childers <chip.childers@gmail.com>
2014-01-06 10:06:29 -0500

    Cleaning up LED program a bit

--- PlayingWithLights/src/sketch.ino
+++ PlayingWithLights/src/sketch.ino
@@ -1,50 +1,43 @@
-/*
+// Set the Digital PIN numbers
+const int RGB_RED_LED_PIN = 9;
+const int RGB_GREEN_LED_PIN = 10;
+const int RGB_BLUE_LED_PIN = 11;
+const int BLUE_LED_PIN = 2;
 
-  RGB_LED_Color_Fade_Cycle.pde
-  
-  Cycles through the colors of a RGB LED
-
-  Written for SparkFun Arduino Inventor's Kit CIRC-RGB
-
-*/
-
-// LED leads connected to PWM pins
-const int RED_LED_PIN = 9;
-const int GREEN_LED_PIN = 10;
-const int BLUE_LED_PIN = 11;
-const int VERYBLUE_LED_PIN = 2;
-
-// Used to store the current intensity level of the individual LEDs
+// Init RGB LED color intensity values
 int redIntensity = 0;
 int greenIntensity = 0;
 int blueIntensity = 0;
 
-// Length of time we spend showing each color
-const int DISPLAY_TIME = 100; // In milliseconds
-
+// Color change time in ms
+const int DISPLAY_TIME = 100;
 
 void setup() {
-  pinMode(VERYBLUE_LED_PIN,OUTPUT);
-  // No setup required.
+  // Setup pin mode for the blue LED's pin
+  pinMode(BLUE_LED_PIN,OUTPUT);
 }
 
+// Simple function used to blink the blue LED
 void blinkblue(int count){
   for (int i=0; i<count; i++){
-    digitalWrite(VERYBLUE_LED_PIN, HIGH);
+    digitalWrite(BLUE_LED_PIN, HIGH);
     delay(DISPLAY_TIME);
-    digitalWrite(VERYBLUE_LED_PIN, LOW);
+    digitalWrite(BLUE_LED_PIN, LOW);
     delay(DISPLAY_TIME);
   }
 }
+
+// Main loop function
 void loop() {
   
   blinkblue(5);
+
   // Cycle color from red through to green
   // (In this loop we move from 100% red, 0% green to 0% red, 100% green)
   for (greenIntensity = 0; greenIntensity <= 255; greenIntensity+=5) {
         redIntensity = 255-greenIntensity;
-        analogWrite(GREEN_LED_PIN, greenIntensity);
-        analogWrite(RED_LED_PIN, redIntensity);
+        analogWrite(RGB_GREEN_LED_PIN, greenIntensity);
+        analogWrite(RGB_RED_LED_PIN, redIntensity);
         delay(DISPLAY_TIME);
   }
 
@@ -54,18 +47,19 @@ void loop() {
   // (In this loop we move from 100% green, 0% blue to 0% green, 100% blue)  
   for (blueIntensity = 0; blueIntensity <= 255; blueIntensity+=5) {
         greenIntensity = 255-blueIntensity;
-        analogWrite(BLUE_LED_PIN, blueIntensity);
-        analogWrite(GREEN_LED_PIN, greenIntensity);
+        analogWrite(RGB_BLUE_LED_PIN, blueIntensity);
+        analogWrite(RGB_GREEN_LED_PIN, greenIntensity);
         delay(DISPLAY_TIME);
   }
 
   blinkblue(5);
+
   // Cycle cycle from blue through to red
   // (In this loop we move from 100% blue, 0% red to 0% blue, 100% red)    
   for (redIntensity = 0; redIntensity <= 255; redIntensity+=5) {
         blueIntensity = 255-redIntensity;
-        analogWrite(RED_LED_PIN, redIntensity);
-        analogWrite(BLUE_LED_PIN, blueIntensity);
+        analogWrite(RGB_RED_LED_PIN, redIntensity);
+        analogWrite(RGB_BLUE_LED_PIN, blueIntensity);
         delay(DISPLAY_TIME);
   }
 
